223db393dd56ed61f4829f606184f558261f6eff
Jorge Villalobos <jorge.villalobos@autodesk.com>
2024-10-07 23:15:00 -0400

    chore(draw): only draw zen, corne not needed

--- scripts/draw.sh
+++ scripts/draw.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 keymap -c ./keymap-drawer/config.yaml parse -z ./config/${1}.keymap >./keymap-drawer/${1}.yaml
-keymap -c ./keymap-drawer/config.yaml draw ./img/${1}.yaml >./keymap-drawer/${1}.svg
+keymap -c ./keymap-drawer/config.yaml draw ./keymap-drawer/${1}.yaml >./keymap-drawer/${1}.svg
