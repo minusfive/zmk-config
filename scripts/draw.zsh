999bb8748b96e5e3bac3b7db6a1dddc1eceb934c
Jorge Villalobos <mail@minusfive.com>
2025-03-22 09:16:27 -0400

    docs: document implicit hyper key

--- scripts/draw.zsh
+++ scripts/draw.zsh
@@ -9,9 +9,10 @@ function {
   local __keymap_input_file="$__repo_dir/config/${__keymap}.keymap"
   local __keymap_svg="$__repo_dir/img/${__keymap}.svg"
   local __keymap_yaml="$__repo_dir/img/${__keymap}.yaml"
+  local __combos_yaml="$__repo_dir/keymap-drawer/combos.yaml"
   local __draw_config="$__repo_dir/keymap-drawer/config.yaml"
 
-  keymap -c $__draw_config parse -z $__keymap_input_file -o $__keymap_yaml
-  keymap -c $__draw_config draw $__keymap_yaml -o $__keymap_svg
+  keymap -c $__draw_config parse -z $__keymap_input_file > $__keymap_yaml
+  keymap -c $__draw_config draw $__keymap_yaml $__combos_yaml > $__keymap_svg
 } $@
 
